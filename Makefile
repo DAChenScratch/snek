fc72de0ff62b524d72147e73255d9daf3c631c6d
aleksiy3252 <aleksiy.kudelsky@rdbrck.com>
2017-02-09 15:21:02 -0800

    added port commandline

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 default:
 	g++ -std=c++11 snek.cpp -o snek -w -lpthread -lboost_system 
 
-
 clean:
 	rm ./snek
+
+run: 
+	./snek 7000 & ./snek 8000 & ./snek 9000
+
+kill:
+	killall snek
--- snek.cpp
+++ snek.cpp
@@ -4,12 +4,37 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdlib>
 
 
 using namespace std;
 using namespace crow;
 using JSON = nlohmann::json;
 
+
+string moveResponse(int dir) {
+	JSON move;
+	switch (dir) {
+	case NORTH: 
+		move["move"] = "north";
+		move["taunt"] = "THE NORTH REMEMBERS";
+		break;
+	case EAST: 
+		move["move"] = "east";
+		move["taunt"] = "TO THE EAST";
+		break;
+	case SOUTH: 
+		move["move"] = "south";
+		move["taunt"] = "SOUTH WHERE ITS WARM";
+		break;
+	case 
+		WEST: move["move"] = "west";
+		move["taunt"] = "WEST IS BEST";
+		break;
+	}
+
+	return move.dump();
+}
 
 string SnakeInfo() {
 	JSON info;
@@ -18,7 +43,7 @@ string SnakeInfo() {
 	return info.dump();
 }
 
-SimpleApp initSnakeApp(){
+SimpleApp initSnakeApp() {
 	SimpleApp app;
 
 	//INFO
@@ -30,7 +55,9 @@ SimpleApp initSnakeApp(){
 	CROW_ROUTE(app, "/start")
 	.methods("POST"_method)
 	([](const crow::request & req) {
-		return "START";
+		JSON taunt;
+		taunt["taunt"] = "Test Taunt";
+		return taunt.dump();
 	});
 
 	//MOVE
@@ -38,7 +65,8 @@ SimpleApp initSnakeApp(){
 	.methods("POST"_method)
 	([](const crow::request & req) {
 		GameInfo game = GameInfo(req.body);
-		return "MOVE";
+		int i = rand() % 4;
+		return moveResponse(i);
 	});
 
 
@@ -46,14 +74,18 @@ SimpleApp initSnakeApp(){
 	CROW_ROUTE(app, "/end")
 	.methods("POST"_method)
 	([](const crow::request & req) {
-		return "END";
+		return "{}";
 	});
 
 	return app;
 }
 
-int main()
+int main(int argc, char **argv)
 {	
+	int port = 7000;
+	if(argc == 2){
+		port = atoi(argv[1]);
+	}
 	SimpleApp app = initSnakeApp();
-	app.port(7000).multithreaded().run();
+	app.port(port).multithreaded().run();
 }
