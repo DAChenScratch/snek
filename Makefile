330b6f7aece403212e18bc9a2a719c6fdf633b16
aleksiy3252 <aleksiy.kudelsky@rdbrck.com>
2017-02-09 10:55:37 -0800

    info is now submitted

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 default:
-	g++ -std=c++11 snek.cpp crow_all.h -o snek -w -lpthread -lboost_system 
+	g++ -std=c++11 snek.cpp -o snek -w -lpthread -lboost_system 
 
 
 clean:
--- snek.cpp
+++ snek.cpp
@@ -1,12 +1,47 @@
 #include "crow/amalgamate/crow_all.h"
+#include <iostream>
+
+using namespace std;
+using namespace crow;
+
+
 
 int main()
 {
-    crow::SimpleApp app;
+    SimpleApp app;
 
+    //INFO
     CROW_ROUTE(app, "/")([](){
-        return "Hello world";
+    	crow::json::wvalue info;
+    	info["color"] = "#FF0000";
+    	info["head"] = "";
+    	return info; 
     });
 
-    app.port(18080).multithreaded().run();
+    //START
+    CROW_ROUTE(app, "/start")
+    .methods("POST"_method)
+    ([](const crow::request& req){
+    	cout << "START" << endl;
+        return "START";
+    });
+
+    //MOVE
+    CROW_ROUTE(app, "/move")
+    .methods("POST"_method)
+    ([](const crow::request& req){
+    	cout << "MOVE" << endl;
+        return "MOVE";
+    });
+
+
+    //END
+    CROW_ROUTE(app, "/end")
+    .methods("POST"_method)
+    ([](const crow::request& req){
+    	cout << "END" << endl;
+        return "END";
+    });
+
+    app.port(7000).multithreaded().run();
 }
